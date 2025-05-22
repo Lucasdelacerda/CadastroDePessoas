465e9f0cdcdeebd87367bd9e9a835e36427bbad3
Lucas de Lacerda <106289445+Lucasdelacerda@users.noreply.github.com>
2025-05-22 15:12:53 -0300

    feat/Controller

---
+++ src/main/java/dev/ScrimetEnterprise/CadastroDePessoas/Controller.java
@@ -0,0 +1,19 @@
+package dev.ScrimetEnterprise.CadastroDePessoas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//essas duas anotações andam juntas
+@RestController
+@RequestMapping
+
+public class Controller {
+    //ele vai pegar a rota e deixar registrado
+    @GetMapping("/boasvindas")
+    public String boasVindas() {
+
+        return "essa é minha primeira mensagem nessa rota";
+    }
+
+}
